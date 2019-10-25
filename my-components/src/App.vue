81bc43226834384797536bab29f4c4bbb5f8d102
dingzhengru <qws7869vdx@gmail.com>
2019-10-26 04:12:24 +0800

    update pagination with $emit & test data

--- my-components/src/App.vue
+++ my-components/src/App.vue
@@ -2,23 +2,37 @@
     <div id="app">
         <h1>Pagination</h1>
         <Pagination 
-        :currentPage="1"
-        :pageSize="2"
-        :data="pageData"></Pagination>
+        class="page"
+        :currentPage="currentPage"
+        :pageSize="pageSize"
+        :data="pageData"
+        @change-page="changePage"></Pagination>
+
+        <table class="table">
+            <tr v-for="(d, index) in pageFilter" v-bind:key="index">
+                <td>
+                    {{ d.id }}
+                </td>
+                <td>
+                    {{ d.name }}
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
 import Pagination from './components/Pagination.vue'
 // import $ from 'jquery'
-import _ from 'lodash'
+// import _ from 'lodash'
 
-console.log(_.cloneDeep);
 
 export default {
     name: 'app',
     data: function() {
         return {
+            currentPage: 1,
+            pageSize: 2,
             pageData: [
                 {
                     id: '1',
@@ -37,11 +51,21 @@ export default {
     },
     components: {
         Pagination
+    },
+    computed: {
+        pageFilter: function() {
+            let startAt = this.pageSize * (this.currentPage - 1);
+            let endAt = startAt + this.pageSize;
+            return this.pageData.slice(startAt, endAt);
+        }
+    },
+    methods: {
+        changePage: function(currentPage) {
+            this.currentPage = currentPage;
+        }
     }
 }
 </script>
-
-
 
 <style>
 #app {
@@ -50,6 +74,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+.page {
+    
 }
 </style>
--- my-components/src/components/Pagination.vue
+++ my-components/src/components/Pagination.vue
@@ -4,6 +4,23 @@
             <!-- currentPage <= 1 ? 0 : currentPage-- 前者為true會執行的 後者為false會執行的 -->
             <ul class="pagination">
                 <li class="page-item"
+                    v-bind:class="{ 'disabled': currentPage <= 1 }"
+                    @click="prevPage()">
+                    <a class="page-link">Previous</a>
+                </li>
+                <li v-for="(p, index) in totalPage(data)"
+                    class="page-item"
+                    v-bind:key="p"
+                    @click="changePage(index + 1)">
+                    <a v-bind:class="{'disabled': currentPage == index + 1}" class="page-link btn" >{{ index + 1 }}</a>
+                </li>
+                <li class="page-item" 
+                    v-bind:class="{ 'disabled': currentPage >= totalPage(data) }"
+                    @click="nextPage()">
+                    <a class="page-link">Next</a>
+                </li>
+
+                <!-- <li class="page-item"
                     v-bind:class="{ 'disabled': cPage <= 1 }"
                     @click="cPage <= 1 ? 0 : cPage--">
                     <a class="page-link">Previous</a>
@@ -18,7 +35,7 @@
                     v-bind:class="{ 'disabled': cPage >= totalPage(data) }"
                     @click="cPage >= totalPage(data) ? 0 : cPage++">
                     <a class="page-link">Next</a>
-                </li>
+                </li> -->
             </ul>
         </nav>
         <p>currentPage: {{ currentPage }}</p>
@@ -50,6 +67,19 @@ export default {
             // ceil 無條件進位
             return Math.ceil(data.length / this.pageSize);
             // return Math.ceil(this.searchProductsByAll(this.searchText).length / this.pageSize);
+        },
+        prevPage: function() {
+            if(this.currentPage <= 1)
+                return
+            this.$emit('change-page', this.currentPage - 1);
+        },
+        nextPage: function() {
+            if(this.currentPage >= this.totalPage(this.data))
+                return
+            this.$emit('change-page', this.currentPage + 1);
+        }, 
+        changePage: function(page) {
+            this.$emit('change-page', page);
         }
     }
 };
